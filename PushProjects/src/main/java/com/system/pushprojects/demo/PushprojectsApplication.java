8bb519b263126bbbe6709269290e3bc7c921e232
SuoSou <suosou@localhost.localdomain>
2020-04-29 15:37:40 +0800

    环境布置

---
+++ PushProjects/src/main/java/com/system/pushprojects/demo/PushprojectsApplication.java
@@ -0,0 +1,13 @@
+package com.system.pushprojects.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushprojectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushprojectsApplication.class, args);
+    }
+
+}
