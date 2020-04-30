056f0f315b3bef24df0899394c1d9603553a9a9c
Suo <2336848356@qq.com>
2020-04-30 09:56:55 +0800

    传输代码[C

--- TempFile/node.js
+++ TempFile/node.js
@@ -1,2 +1,20 @@
-1111:
-33
+var express = require("express");
+var router = express.Router();
+var base = require("../base");
+var arrondi_productlogic = require("../../logic/mall/arrondi_productlogic");
+const productservice = require("../../logic/mall/productservice");
+router.all('/', function (req, res) {
+    base.execute(req.body.param, arrondi_productlogic, req)
+        .then(function (result) {
+            res.send(result);
+        })
+})
+// 查询指定专区的商品
+router.all('/getspecialareagoodslist', function (req, res) {
+    base.execute(req.body.param, productservice, req)
+        .then(function (result) {
+            res.send(result);
+        })
+})
+
+module.exports = router;
